049352280e2d89329d4f276f3604f27338a8d34b
github username <github email address>
2017-10-10 20:50:43 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+require './config/environment'
+require 'sinatra/activerecord/rake'
+
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/dogs.db'
+end
 
 require './app'
